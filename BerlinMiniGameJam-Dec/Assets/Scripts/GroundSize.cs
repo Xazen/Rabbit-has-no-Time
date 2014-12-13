50ebd797492ebba60d70c9465f827c8d89764882
Dennis Lieu <dennis.lieu@xazen.de>
2014-12-13 15:33:33 +0100

    Add ground

---
+++ BerlinMiniGameJam-Dec/Assets/Scripts/GroundSize.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundSize : MonoBehaviour {
+
+	[SerializeField]
+	public float groundHeight = 1;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = new Vector3(this.transform.localScale.x, groundHeight, this.transform.localScale.z);
+	}
+}
