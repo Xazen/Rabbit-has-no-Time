0c47d987ae4dfc9f5779deed404c3d1e6afd2dbb
Dennis Lieu <dennis.lieu@xazen.de>
2014-12-13 17:21:03 +0100

    - Add carrot UI

--- BerlinMiniGameJam-Dec/Assets/Scripts/CarrotCollector.cs
+++ BerlinMiniGameJam-Dec/Assets/Scripts/CarrotCollector.cs
@@ -5,7 +5,7 @@ public class CarrotCollector : MonoBehaviour
 {
 	[SerializeField]
 	float carrotTimer = 4.0f;
-	float requiredCarrotsForAttacking = 4.0f;
+	int requiredCarrotsForAttacking = 4;
 	
 	private int carrotCounter = 0;
 
