33dbac3b6bb710660842d0b73b02ed67dfe05b4c
Dennis Lieu <dennis.lieu@xazen.de>
2014-12-13 16:23:37 +0100

    Falling Carrots

---
+++ BerlinMiniGameJam-Dec/Assets/CarrotCollector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarrotCollector : MonoBehaviour 
+{
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.name == "Carrot(Clone)") 
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ BerlinMiniGameJam-Dec/Assets/CarrotSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarrotSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject carrot;
+	[SerializeField]
+	private float spawnFrequency = 3;
+	// Use this for initialization
+
+	private float spawnTimer = 0;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnTimer += Time.deltaTime;
+		
+		if (spawnTimer >= spawnFrequency) 
+		{
+			spawnTimer = 0;
+			GameObject spawningCarrot = (GameObject)GameObject.Instantiate(carrot);
+			float randomX = Random.Range(0, 20);
+			spawningCarrot.transform.localPosition = new Vector3(randomX, 10);
+		}
+
+	}
+}
