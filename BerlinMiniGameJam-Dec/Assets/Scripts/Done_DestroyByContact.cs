7c6cedd672c3142ed0d73fee78504d79a4d74746
Mantirius <addy.nagel@googlemail.com>
2014-12-13 15:30:47 +0100

    Scripts Update
    Scripts Update for Wolf, Player (Move, Controller)
    add Game Controller

---
+++ BerlinMiniGameJam-Dec/Assets/Scripts/Done_DestroyByContact.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private Done_GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		//Animation Wolf Punching - delay of 3 Seconds to next Punch
+		if (other.tag == "Enemy")
+		{
+			return;
+		}
+
+		if (explosion != null)
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+		}
+		
+		gameController.AddScore(scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ BerlinMiniGameJam-Dec/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Mover : MonoBehaviour {
+
+	public GameObject player;
+	//player
+	public float speed; //declare the player move speed in Unity inspector
+	public Transform groundedEnd; //declares the empty game object in Unity acting as a collider set to the position of the player
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				Movement (); //call the movement function below
+				isGrounded = Physics2D.Linecast (this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer ("Ground")); 
+		}
+	void Movement()
+		{
+			//Move Right
+			if (Input.GetKey (KeyCode.LeftArrow)) 
+			{
+				transform.Translate (Vector2.left * speed * Time.deltaTime);
+				transform.eulerAngles = new Vector2(0,0); 
+			}
+			//Move Left
+			if (Input.GetKey (KeyCode.RightArrow)) 
+			{
+				transform.Translate (Vector2.right * speed * Time.deltaTime);
+				transform.eulerAngles = new Vector2(0,180); //flip the character on its x axis
+			}			
+		}
+}
